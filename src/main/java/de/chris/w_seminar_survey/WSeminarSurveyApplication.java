c121a2db4f407a13bbb184b75b23b7be10abaa77
ChristopherEickhoff <christopher.eickhoff@icloud.com>
2025-09-15 11:05:48 +0200

    spring initializr

---
+++ src/main/java/de/chris/w_seminar_survey/WSeminarSurveyApplication.java
@@ -0,0 +1,13 @@
+package de.chris.w_seminar_survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WSeminarSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WSeminarSurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/chris/w_seminar_survey/WSeminarSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.chris.w_seminar_survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WSeminarSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
